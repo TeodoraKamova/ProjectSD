10c127ddef2237ec2c02c73392276587e85c2ba7
TeodoraKamova <105005855+TeodoraKamova@users.noreply.github.com>
2022-06-10 07:33:51 +0300

    Add files via upload
    Init - Business Layer

---
+++ BusinessLayer/Human.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BusinessLayer
+{
+    public abstract class Human
+    {
+        [Key]
+        public int Id { get; private set; }
+        [Required]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Surname { get; set; }
+        [Required]
+        [Range(0, 130, ErrorMessage = "Age must be between 0 and 130")]
+        public int Age { get; set; }
+
+        protected Human(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+        protected Human()
+        {
+
+        }
+    }
+}
---
+++ BusinessLayer/Pateint.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BusinessLayer
+{
+    public class Pateint : Human
+    {
+        public enum BloodTypes
+        {
+           A,
+           B,
+           O,
+           AB
+        }
+        [Required]
+        public BloodTypes BloodType { get; set; }
+        [Required]
+        public Doctor Doctor { get; set; }
+        [ForeignKey("Doctor")]
+        public int DocId { get; set; }
+        public Sickness Sickness { get; set; }
+        [ForeignKey("Sickness")]
+        public int SicknessId { get; set; }
+        public Pateint(string name, string surname, int age, BloodTypes bloodType, Doctor doctor) : base(name, surname, age)
+        {
+            BloodType = bloodType;
+            Doctor = doctor;
+        }
+        private Pateint()
+        {
+
+        }
+    }
+}
